0d69dcabf578d69eb2bc735bf17331ae2a34c9c4
Robert Hoffmann <roho@uhurutec.com>
2025-08-21 08:41:31 +0200

    -

--- cloudmock/openstack/mockcompute/servers.go
+++ cloudmock/openstack/mockcompute/servers.go
@@ -47,7 +47,7 @@ type ExtendedServerType struct {
 	servers.Server
 	Created    JSONRFC3339MilliNoZ `json:"-"`
 	Updated    JSONRFC3339MilliNoZ `json:"-"`
-	LaunchedAt JSONRFC3339MilliNoZ `json:"launched_at"`
+	LaunchedAt JSONRFC3339MilliNoZ `json:"-"`
 }
 
 type serverGetResponse struct {
